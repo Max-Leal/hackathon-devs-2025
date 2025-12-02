5ee86c3343e114159513a126ed051d2abaeff49e
Antônio Rocha da Silva <antoniorochadasilvaa@gmail.com>
2025-12-02 10:16:57 -0300

    hotfix: re-implementando backend em maven

--- backend/src/main/java/com/hackweek/scorebanking/ScoreBankingApplication.java
+++ backend/src/main/java/com/hackweek/scorebanking/ScoreBankingApplication.java
@@ -1,4 +1,4 @@
-package com.hackathon.ScoreBanking;
+package com.hackweek.scorebanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScoreBankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScoreBankingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreBankingApplication.class, args);
+    }
 
 }
