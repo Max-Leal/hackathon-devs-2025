6cc82f5adcd2c8352e678cb540d38c1ffd0a388c
Max-leal <Max13leal@gmail.com>
2025-12-03 14:56:55 -0300

    Arrumado docker-compose e rodando na develop

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -9,13 +9,14 @@ resource "aws_instance" "instance-scorebanking" {
   user_data = base64encode(<<-EOF
     #!/bin/bash
     apt-get update
-    apt install -y docker.io git build-essential
+    apt install -y docker.io git build-essential docker-compose
     systemctl start docker
     systemctl enable docker
+
     cd /root
     git clone https://github.com/Max-Leal/hackathon-devs-2025.git
     cd ./hackathon-devs-2025
-    git checkout feature/terraform
+    git checkout develop
     make docker-build
     make docker-run      
   EOF
