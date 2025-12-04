a74c938206c0fe6c55062e025a963689102c0376
Antônio Rocha da Silva <antoniorochadasilvaa@gmail.com>
2025-12-04 20:25:27 -0300

    fix: ajustado de novo

--- backend/src/main/java/com/hackweek/scorebanking/domain/RiskTier.java
+++ backend/src/main/java/com/hackweek/scorebanking/domain/RiskTier.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public enum RiskTier {
     EXCELLENT(new BigDecimal("0.0175"), 48),
     GOOD(new BigDecimal("0.0210"), 36),
-    MEDIUM(new BigDecimal("0.320"), 36),
+    MEDIUM(new BigDecimal("0.0320"), 36),
     HIGH(new BigDecimal("0.0415"), 24), 
     TERRIBLE(BigDecimal.ZERO, 0);
 
